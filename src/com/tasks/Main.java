6c029f2b90f6028d69544966b3a9c41c5e9ad38a
robert-melena <99871728+robert-melena@users.noreply.github.com>
2026-02-06 03:35:24 -0800

    First commit

---
+++ src/com/tasks/Main.java
@@ -0,0 +1,11 @@
+package com.tasks;
+
+public class Main {
+    public static void main(String[] args) {
+        if(args.length == 0){
+            System.out.println("No args available");
+        }
+
+        System.out.println("Sup");
+    }
+}
---
+++ src/com/tasks/Task.java
@@ -0,0 +1,19 @@
+package com.tasks;
+
+import java.time.LocalDateTime;
+
+public class Task {
+    private static int ID = 1;
+
+    private int taskId;
+    private String description;
+    private String status;
+    private LocalDateTime localDateTime;
+    private LocalDateTime updateTime;
+
+    public Task(String description, String status){
+
+
+    }
+
+}
